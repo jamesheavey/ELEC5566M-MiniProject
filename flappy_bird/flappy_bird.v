82f81d42bb0ba6e369d66506e39ff6ad8a4a0893
James Heavey <76979225+jamesheavey@users.noreply.github.com>
2021-05-20 04:50:24 +0100

    GitHub Formatting

--- flappy_bird/flappy_bird.v
+++ flappy_bird/flappy_bird.v
@@ -14,12 +14,10 @@
  *
  * Use
  * ---------------------------------
- * This module was designed for implementation
- * on the DE1-SoC board with pin assignments 
- * detailed in "pin_config.csv". A VGA monitor/
- * TV must be connected to the board for image
- * display, along with a PS2 keyboard for character
- * control.
+ * This module was designed for implementation on the DE1-SoC board 
+ * with pin assignments detailed in "pin_config.csv". A VGA monitor/
+ * TV must be connected to the board for image display, along with a
+ * PS2 keyboard for character control.
  *
  */
 
@@ -39,13 +37,13 @@ module flappy_bird
 
 
 // PARAMETERS
-localparam SCALE 		=   3;
+localparam SCALE 	=   3;
 localparam PIPE_GAP 	=  75;
 localparam NUM_PIPES 	=   4;
-localparam PIPE_SEP		= 250;
+localparam PIPE_SEP	= 250;
 localparam BIRD_SIZE_X 	= 18*SCALE; 
 localparam BIRD_SIZE_Y 	= 15*SCALE;
-localparam BIRD_X		= 150;
+localparam BIRD_X	= 150;
 
 
 // 25MHz clk required by VGA 640x480 timing 
@@ -72,156 +70,139 @@ wire [11:0] score_BCD, hiscore_BCD;
 wire flap, pause, collision;
 
 
-// Instantiation of the vga generator module.
-// This module outputs the relevant VGA port outputs 
-// as well as the current update pixel coordinates (X,Y).
-// These coordinates are used by the image renderer to time
-// sprite display.
+// Instantiation of the vga generator module. This module outputs the relevant VGA port
+// outputs as well as the current update pixel coordinates (X,Y). These coordinates are
+// used by the image renderer to time sprite display.
 vga_gen vga
 (
-	.clk			( VGA_clk 		),
-	.rst			( rst 			),
-	.h_sync			( h_sync 		),
-	.v_sync			( v_sync 		),
-	.v_clk			( v_clk 		),
-	.display_on		( display_on 	),
-	.sync_n			( sync_n 		),
-	.h_pos			( X 			),
-	.v_pos			( Y 			)
+	.clk		( VGA_clk 	),
+	.rst		( rst 		),
+	.h_sync		( h_sync 	),
+	.v_sync		( v_sync 	),
+	.v_clk		( v_clk 	),
+	.display_on	( display_on 	),
+	.sync_n		( sync_n 	),
+	.h_pos		( X 		),
+	.v_pos		( Y 		)
 );
 
 
-// Instantiation of the PS2 keyboard input module.
-// This module reads the output from a connected PS2
-// keyboard and decodes the signal into 2 keypresses:
-// space = "flap", esc = "pause".
+// Instantiation of the PS2 keyboard input module. This module reads the output from a
+// connected PS2 keyboard and decodes the signal into 2 user inputs.
 keyboard_input kb
 (
-	.clk			( clk 			),
-	.rst			( rst 			),
-	.PS2_clk		( PS2_clk 		),
-	.PS2_data		( PS2_data 		),
-	.flap			( flap 			),
-	.pause			( pause 		)
+	.clk		( clk 		),
+	.rst		( rst 		),
+	.PS2_clk	( PS2_clk 	),
+	.PS2_data	( PS2_data 	),
+	.flap		( flap 		),
+	.pause		( pause 	)
 );
 
 
-// Instantiation of the game FSM module.
-// This module defines the current game state
-// and transitions based on user inputs or game
-// events. The current game state is output to 
+// Instantiation of the game FSM module. This module defines the current game state
+// and transitions based on user inputs or game events. The current game state is output to 
 // determine functions within other modules.
 game_FSM FSM
 (
-	.clk			( clk 			),
-	.rst			( rst 			),
-	.collision		( collision 	),
-	.pause			( pause 		),
-	.flap			( flap 			),
-	.game_state		( game_state 	)
+	.clk		( clk 		),
+	.rst		( rst 		),
+	.collision	( collision 	),
+	.pause		( pause 	),
+	.flap		( flap 		),
+	.game_state	( game_state 	)
 );
 
 
-// Instantiation of the bird physics module.
-// This module defines the motion of the bird
-// character. The bird Y coordinate is output 
-// for use in image rendering and collision.
+// Instantiation of the bird physics module. This module defines the motion of the bird
+// character. The bird Y coordinate is output for use in image rendering and collision.
 bird_physics #(
 	.BIRD_SIZE_X	( BIRD_SIZE_X 	),
 	.BIRD_SIZE_Y	( BIRD_SIZE_Y 	)
 ) physics (
-	.clk			( clk 			),
-	.rst			( rst			),
-	.game_state		( game_state 	),
-	.flap			( flap 			),
-	.birdY			( birdY 		),
-	.bird_state		( bird_state 	),
-	.bird_angle		( bird_angle	)
+	.clk		( clk 		),
+	.rst		( rst		),
+	.game_state	( game_state 	),
+	.flap		( flap 		),
+	.birdY		( birdY 	),
+	.bird_state	( bird_state 	),
+	.bird_angle	( bird_angle	)
 );
 
 
-// Instantiation of the collision detetcion module.
-// This module uses the current bird and pipe coordinates
-// to determine if any sprites overlap. if they do,
-// collision goes high.
+// Instantiation of the collision detetcion module. This module uses the current bird and
+// pipe coordinates to determine if any sprites overlap. if they do, collision goes high.
 collision_detection #(
 	.BIRD_SIZE_X	( BIRD_SIZE_X 	),
 	.BIRD_SIZE_Y	( BIRD_SIZE_Y 	),
-	.PIPE_GAP		( PIPE_GAP		),
-	.NUM_PIPES		( NUM_PIPES		)
+	.PIPE_GAP	( PIPE_GAP	),
+	.NUM_PIPES	( NUM_PIPES	)
 ) detect (
-	.clk			( clk 			),
-	.birdX			( birdX 		),
-	.birdY			( birdY 		),
-	.pipeX_flat		( pipeX_flat	),
-	.pipeY_flat		( pipeY_flat	),
-	.collision		( collision		)
+	.clk		( clk 		),
+	.birdX		( birdX 	),
+	.birdY		( birdY 	),
+	.pipeX_flat	( pipeX_flat	),
+	.pipeY_flat	( pipeY_flat	),
+	.collision	( collision	)
 );
 
 
-// Instantiation of the pipe controller module.
-// This module creates and moves the pipe obstacles
-// for the game. The number and separation of pipes
-// are defined as input params.
+// Instantiation of the pipe controller module. This module creates and moves the pipe obstacles
+// for the game. The number and separation of pipes are defined as input params.
 pipes #(
-	.NUM_PIPES		( NUM_PIPES		),
-	.PIPE_SEP		( PIPE_SEP		)
+	.NUM_PIPES	( NUM_PIPES	),
+	.PIPE_SEP	( PIPE_SEP	)
 ) pipe_shift (
-	.clk			( clk 			),
-	.FL_clk			( FL_clk		),
-	.rst			( rst			),
+	.clk		( clk 		),
+	.FL_clk		( FL_clk	),
+	.rst		( rst		),
 	.game_state 	( game_state	),
-	.birdX			( birdX 		),
-	.pipeX_flat		( pipeX_flat	),
-	.pipeY_flat		( pipeY_flat	),
+	.birdX		( birdX 	),
+	.pipeX_flat	( pipeX_flat	),
+	.pipeY_flat	( pipeY_flat	),
 	.score_count	( score_count	)
 );
 
 
-// Instantiation of the score counter module.
-// This module tracks the current score and hiscore,
-// transforming them to BCD for display on the screen
-// and 7 segments.
+// Instantiation of the score counter module. This module tracks the current score and hiscore,
+// transforming them to BCD for display on the screen and 7 segments.
 score_display score
 (
-	.rst			( rst			),
+	.rst		( rst		),
 	.score_count	( score_count	),
-	.score_BCD		( score_BCD		),
+	.score_BCD	( score_BCD	),
 	.hiscore_BCD	( hiscore_BCD	),
-	.seven_seg		( seven_seg		)
+	.seven_seg	( seven_seg	)
 );
 
 
-// Instantiation of the image renderer module.
-// This module outs a 24-bit RGB pixel colour at
-// every VGA clk cycle. The colour is selected via
-// logic using the VGA pixel coordinates (X,Y),
-// the object positions and sizes, and the display
-// priority of each sprite.
+// Instantiation of the image renderer module. This module outs a 24-bit RGB pixel colour at
+// every VGA clk cycle. The colour is selected via logic using the VGA pixel coordinates (X,Y),
+// the object positions and sizes, and the display priority of each sprite.
 image_rendering #(
 	.BIRD_SIZE_X	( BIRD_SIZE_X 	),
 	.BIRD_SIZE_Y	( BIRD_SIZE_Y 	),
-	.SCALE			( SCALE 		),
-	.PIPE_GAP		( PIPE_GAP		),
-	.NUM_PIPES		( NUM_PIPES		)
+	.SCALE		( SCALE 	),
+	.PIPE_GAP	( PIPE_GAP	),
+	.NUM_PIPES	( NUM_PIPES	)
 ) display (
-	.VGA_clk		( VGA_clk 		),
-	.BG_clk			( BG_clk 		),
-	.FL_clk			( FL_clk		),
-	.rst			( rst 			),
-	.display_on		( display_on 	),
-	.game_state		( game_state 	),
-	.bird_state		( bird_state 	),
-	.bird_angle		( bird_angle	),
-	.X				( X 			),
-	.Y				( Y 			),
-	.birdX			( birdX 		),
-	.birdY			( birdY 		),
-	.pipeX_flat		( pipeX_flat	),
-	.pipeY_flat		( pipeY_flat	),
-	.score_BCD		( score_BCD		),
+	.VGA_clk	( VGA_clk 	),
+	.BG_clk		( BG_clk 	),
+	.FL_clk		( FL_clk	),
+	.rst		( rst 		),
+	.display_on	( display_on 	),
+	.game_state	( game_state 	),
+	.bird_state	( bird_state 	),
+	.bird_angle	( bird_angle	),
+	.X		( X 		),
+	.Y		( Y 		),
+	.birdX		( birdX 	),
+	.birdY		( birdY 	),
+	.pipeX_flat	( pipeX_flat	),
+	.pipeY_flat	( pipeY_flat	),
+	.score_BCD	( score_BCD	),
 	.hiscore_BCD	( hiscore_BCD	),
-	.RGB			( {R,G,B} 		)
+	.RGB		( {R,G,B} 	)
 );
 
 // Upon collision, the LEDs flash
